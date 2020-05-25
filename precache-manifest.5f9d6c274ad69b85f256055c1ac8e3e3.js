6d3217a98aed1703836de498ec91d841ae921a5f
kirito22037 <kirito22037@gmail.com>
2020-05-25 14:15:35 +0530

    Updates

---
+++ precache-manifest.5f9d6c274ad69b85f256055c1ac8e3e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ef389c9ec66f54f083908b63fcfb0a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a017fc87e587bec023c",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2130cd63bf190f1be454",
+    "url": "/static/js/2.b21cebba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b21cebba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a017fc87e587bec023c",
+    "url": "/static/js/main.4e062f09.chunk.js"
+  },
+  {
+    "revision": "b4ba650b238755144e2a",
+    "url": "/static/js/runtime-main.e5fc83a6.js"
+  }
+]);
